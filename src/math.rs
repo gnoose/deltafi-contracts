75236473b3d694594a6b16bd25545b3a656a32e2
JieFinance <jie@nitaswap.finance>
2021-09-03 21:59:27 +0900

    upt: update date-type f64 to U256  * update all date-type of real functions  * not updated date-type of unit-test

--- src/math.rs
+++ src/math.rs
@@ -1,22 +1,77 @@
 //! Implement DODOMath calculation
 
+use crate::bn::U256;
+
+/// calculate sqrt
+pub fn integer_sqrt(x: U256) -> U256 {
+    let two: U256 = 2.into();
+
+    let mut z: U256 = (x + U256::one()) / two;
+
+    let mut y = x;
+
+    while z < y {
+        y = z;
+        z = (x / z + z) / two;
+    }
+
+    y
+}
+
+/// calculate div ceil
+pub fn div_ceil(
+    value0: U256,
+    value1: U256
+) -> U256 {
+    let result = value0 / value1;
+    if value0 - result * value1 > U256::from(0) {
+        result + U256::from(1)
+    } else {
+        result
+    }
+}
+
+/// calculate div floor
+pub fn div_floor(
+    value0: U256,
+    value1: U256
+) -> U256 {
+    value0 / value1
+}
+
+/// calculate mul ceil
+pub fn mul_ceil(
+    value0: U256,
+    value1: U256
+) -> U256 {
+    value0 * value1
+}
+
+/// calculate mul floor
+pub fn mul_floor(
+    value0: U256,
+    value1: U256
+) -> U256 {
+    value0 * value1
+}
+
 /// integrate curve from v1 to v2  = i * delta * (1 - k + k(v0^2 / v1 /v2))
 //         require V0>=V1>=V2>0
 //         res = (1-k)i(V1-V2)+ikV0*V0(1/V2-1/V1)
 //         let V1-V2=delta
 //         res = i*delta*(1-k+k(V0^2/V1/V2))
 pub fn general_integrate(
-    v0: f64,
-    v1: f64,
-    v2: f64,
-    i: f64,
-    k: f64
-) -> f64 {
+    v0: U256,
+    v1: U256,
+    v2: U256,
+    i: U256,
+    k: U256
+) -> U256 {
     let fair_amount = i * (v1 - v2);
     let v0v0v1v2 = v0 * v0 / v1 / v2;
     let penalty = k * v0v0v1v2;
 
-    fair_amount * (f64::from(1) - k + penalty)
+    fair_amount * (U256::from(1) - k + penalty)
 }
 
 /// The same with integration expression above, we have:
@@ -32,16 +87,16 @@ pub fn general_integrate(
 //         if deltaBSig=true, then Q2>Q1
 //         if deltaBSig=false, then Q2<Q1
 pub fn solve_quadratic_function_for_trade(
-    q0: f64,
-    q1: f64,
-    i_delta_b: f64,
+    q0: U256,
+    q1: U256,
+    i_delta_b: U256,
     delta_b_sig: bool,
-    k: f64
-) -> f64 {
+    k: U256
+) -> U256 {
     // calculate -b value and sig
     // -b = (1-k)Q1-kQ0^2/Q1+i*deltaB
     let mut kq02q1 = k * q0 * q0 / q1;
-    let mut b = (f64::from(1) - k) * q1;
+    let mut b = (U256::from(1) - k) * q1;
 
     let minus_b_sig;
     if delta_b_sig {
@@ -59,11 +114,11 @@ pub fn solve_quadratic_function_for_trade(
     }
 
     // calculate sqrt
-    let mut square_root = f64::from(4) * (f64::from(1) - k) * k * q0 * q0;
-    square_root = (b  * b + square_root).sqrt();
+    let mut square_root = U256::from(4) * (U256::from(1) - k) * k * q0 * q0;
+    square_root = integer_sqrt(b  * b + square_root);
 
     // final res
-    let denominator = f64::from(2) * (f64::from(1) - k);
+    let denominator = U256::from(2) * (U256::from(1) - k);
     let numerator;
     if minus_b_sig {
         numerator = b + square_root;
@@ -71,11 +126,10 @@ pub fn solve_quadratic_function_for_trade(
         numerator = square_root - b;
     }
 
-    let result = numerator / denominator;
     if delta_b_sig {
-        result.floor()
+        div_floor(numerator, denominator)
     } else {
-        result.ceil()
+        div_ceil(numerator, denominator)
     }
 }
 
@@ -84,18 +138,18 @@ pub fn solve_quadratic_function_for_trade(
 //         Assume Q2=Q0, Given Q1 and deltaB, solve Q0
 //         let fairAmount = i*deltaB
 pub fn solve_quadratic_function_for_target (
-    v1: f64,
-    k: f64,
-    fair_amount: f64
-) -> f64 {
+    v1: U256,
+    k: U256,
+    fair_amount: U256
+) -> U256 {
     // V0 = V1+V1*(sqrt-1)/2k
-    let mut sqrt = (k * fair_amount * f64::from(4) / v1).ceil();
-    sqrt = ((sqrt + f64::from(1)) * f64::from(1)).sqrt();
+    let mut sqrt = div_ceil(k * fair_amount * U256::from(4), v1);
+    sqrt = integer_sqrt((sqrt + U256::from(1)) * U256::from(1));
 
-    let premium = ((sqrt - f64::from(1)) / (k * f64::from(2))).ceil();
+    let premium = div_ceil(sqrt - U256::from(1), k * U256::from(2));
 
     // V0 is greater than or equal to V1 according to the solution
-    v1 * (f64::from(1) + premium)
+    v1 * (U256::from(1) + premium)
 }
 
 #[cfg(test)]
